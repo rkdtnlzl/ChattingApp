614944f120a55f8d3d50657cf5322c1c3a008401
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-18 23:16:38 +0900

    [Feat] Travel Talk 메인 ViewController 생성

---
+++ ChattingApp/ChattingListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ChattingListViewController.swift
+//  ChattingApp
+//
+//  Created by 강석호 on 7/18/24.
+//
+
+import UIKit
+
+class ChattingListViewController: BaseViewController {
+    
+    
+}
