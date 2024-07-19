578fbbe8003d708148b94d97097b147b9731dead
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-19 23:17:16 +0900

    [Fix] Configuration 스타일 subtitleCell로 변경

--- ChattingApp/CollectionViewDataSourceManager.swift
+++ ChattingApp/CollectionViewDataSourceManager.swift
@@ -10,13 +10,13 @@ import UIKit
 class CollectionViewDataSourceManager {
     static func createChatDataSource(for collectionView: UICollectionView) -> UICollectionViewDiffableDataSource<Section, Chat> {
         let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Chat> { (cell, indexPath, chat) in
-            var content = UIListContentConfiguration.valueCell()
+            var content = UIListContentConfiguration.subtitleCell()
             content.text = chat.user.rawValue
             content.textProperties.color = .black
             content.textProperties.font = .boldSystemFont(ofSize: 17)
             content.secondaryText = chat.message
             content.image = UIImage(named: "\(chat.user.rawValue)")
-            let imageSize = CGSize(width: 80, height: 80)
+            let imageSize = CGSize(width: 60, height: 60)
             content.imageProperties.reservedLayoutSize = imageSize
             content.imageProperties.maximumSize = imageSize
             content.imageProperties.cornerRadius = 40
